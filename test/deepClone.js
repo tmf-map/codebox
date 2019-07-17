1e55b40083d23d5e1437aa84e78ecf2c3ff44519
Kimi <mrgaonju@gmail.com>
2019-07-17 14:01:09 +0800

    optimize input simple obj and function test

--- test/deepClone.js
+++ test/deepClone.js
@@ -2,24 +2,24 @@ import test from 'ava';
 import deepClone from '../deepClone'
 
 test('input string', t => {
-	t.is(deepClone('abc'), 'abc');
+  t.is(deepClone('abc'), 'abc');
 });
 test('input number', t => {
-	t.is(deepClone(-1980), -1980);
+  t.is(deepClone(-1980), -1980);
 });
 test('input null', t => {
-	t.is(deepClone(null), null);
+  t.is(deepClone(null), null);
 });
 test('input undefined', t => {
-	t.is(deepClone(undefined), undefined);
+  t.is(deepClone(undefined), undefined);
 });
 test('input false', t => {
-	t.is(deepClone(false), false);
+  t.is(deepClone(false), false);
 });
-test('input obj', t => {
+test('input simple obj', t => {
   let obj = {
     a: 1,
-    b: () => console.log(1),
+    b: 'string',
     c: {
       d: 3,
       e: 4
@@ -29,12 +29,27 @@ test('input obj', t => {
     nul: null
   };
   let objCopy = deepClone(obj);
-	t.false(obj === objCopy);
-	t.false(obj.c === objCopy.c);
-	// t.false(obj.b === objCopy.b);
+  t.false(obj === objCopy);
+  t.false(obj.c === objCopy.c);
   t.false(obj.f === objCopy.f);
-  t.is(obj.nul, null);
-  t.is(obj.und, undefined);
+  t.true(JSON.stringify(obj) === JSON.stringify(objCopy));
+  t.true(obj.a === objCopy.a);
+  t.true(obj.b === objCopy.b);
+  t.true(obj.c.d === objCopy.c.d);
+  t.true(obj.und === objCopy.und);
+  t.true(obj.nul === objCopy.nul);
+});
+test('input function', t => {
+  let foo1 = () => console.log(1);
+  let foo2 = function () {
+    console.log(1);
+  }
+  let foo1Copy = deepClone(foo1);
+  let foo2Copy = deepClone(foo2);
+  t.false(foo1 === foo1Copy);
+  t.false(foo2 === foo2Copy);
+  t.true(foo1.toString() === foo1Copy.toString());
+  t.true(foo2.toString() === foo2Copy.toString());
 });
 test('input Date', t => {
   let date = new Date();
