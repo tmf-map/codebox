af320a4ef80939e081e9d0096cdc01a396ca318f
Kimi <mrgaonju@gmail.com>
2019-07-20 23:47:53 +0800

    remove unfinished test

--- test/unique.js
+++
@@ -1,9 +0,0 @@
-import test from 'ava';
-import unique from '../unique'
-
-test('remove duplicates from unsorted array', t => {
-  t.deepEqual(unique([3,2,2,3]), [3, 2]);
-});
-test('remove duplicates from sorted array', t => {
-  t.deepEqual(unique([1,2,2,3]), [1, 2, 3]);
-});
