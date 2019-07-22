d48ddccd10c59405527a4764e6952ca798139256
USTC-Han <839057430@qq.com>
2019-07-22 21:37:00 +0800

    optimize deepEquals test

--- test/deepEquals.js
+++ test/deepEquals.js
@@ -2,66 +2,66 @@ import test from 'ava';
 import deepEqual from '../src/deepEqual'
 
 test('String', t => {
-    t.is(deepEqual('abc','abc'), true);
-    t.is(deepEqual('abc','ab'), false);
+    t.true(deepEqual('abc','abc'));
+    t.false(deepEqual('abc','ab'));
 });
 test('Number', t => {
-    t.is(deepEqual(100, 100), true);
-    t.is(deepEqual(12, 1), false);
+    t.true(deepEqual(100, 100));
+    t.false(deepEqual(12, 1));
 });
 test('null', t => {
-    t.is(deepEqual(null, {}), false);
-    t.is(deepEqual(null, null), true);
-    t.is(deepEqual(null, undefined), false);
+    t.false(deepEqual(null, {}));
+    t.true(deepEqual(null, null));
+    t.false(deepEqual(null, undefined));
 });
 test('undefined', t => {
-    t.is(deepEqual(undefined, undefined), true);
-    t.is(deepEqual(undefined, 0), false);
+    t.true(deepEqual(undefined, undefined));
+    t.false(deepEqual(undefined, 0));
 });
 test('Bool', t => {
-    t.is(deepEqual(false, false), true);
-    t.is(deepEqual(false, true), false);
+    t.true(deepEqual(false, false));
+    t.false(deepEqual(false, true));
 });
 test('Date', t => {
     const a = new Date();
     const b = new Date();
     const c = new Date(+a + 1000);
-    t.is(deepEqual(a, b), true);
-    t.is(deepEqual(a, c), false);
+    t.true(deepEqual(a, b));
+    t.false(deepEqual(a, c));
 });
 test('RagExp', t => {
     const a = new RegExp('test');
     const b = new RegExp('test');
-    t.is(deepEqual(a, b), true);
-    t.is(deepEqual(a, /^test$/), false);
+    t.true(deepEqual(a, b));
+    t.false(deepEqual(a, /^test$/));
 });
 test('Set', t => {
     const a = new Set([1, 'da', null, {a: 'test', b: [1, 'a']}]);
     const b = new Set([1, 'da', null, {a: 'test', b: [1, 'a']}]);
     const c = new Set([1, 'da', null, {a: 'test', b: [1, 'b']}]);
-    t.is(deepEqual(a, b), true);
-    t.is(deepEqual(a, c), false);
+    t.true(deepEqual(a, b));
+    t.false(deepEqual(a, c));
 });
 test('Map', t => {
     const a = new Map([[{a: 'b'}, {c: 'd'}],['test', null]]);
     const b = new Map([[{a: 'b'}, {c: 'd'}],['test', null]]);
     const c = new Map([[{a: 'e'}, {c: 'd'}],['test', null]]);
-    t.is(deepEqual(a, b), true);
-    t.is(deepEqual(a, c), false);
+    t.true(deepEqual(a, b));
+    t.false(deepEqual(a, c));
 });
 test('Array', t => {
     const a = ['a', 1, null, undefined, {a: 'b', b: {c: [1,2,3]}}, new Set(), new Map()];
     const b = ['a', 1, null, undefined, {a: 'b', b: {c: [1,2,3]}}, new Set(), new Map()];
     const c = ['a', 1, null, undefined, new Set(), new Map(), {a: 'b', b: {c: [1,2,3]}}];
     const d = ['a', 1, null, undefined, {a: 'b'}, new Set(), new Map()];
-    t.is(deepEqual(a, b), true);
-    t.is(deepEqual(a, c), false);
-    t.is(deepEqual(b, c), false);
+    t.true(deepEqual(a, b));
+    t.false(deepEqual(a, c));
+    t.false(deepEqual(b, c));
 });
 test('object', t => {
     const a = {a: 'b', b: 'c'};
     const b = {b: 'c', a: 'b'};
     const c = {a: 'b'};
-    t.is(deepEqual(a, b), true);
-    t.is(deepEqual(a, c), false);
+    t.true(deepEqual(a, b));
+    t.false(deepEqual(a, c));
 });
